60c3549241b0d4b392fe4999206756ee9124bf72
Sindon20 <ginessindonjhamaicamae@gmail.com>
2025-02-26 12:16:13 +0800

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,30 @@
+let myFirstName_MiddleName_LastName = "Jhamaica Mae Gines Sindon";
+let myBirthdate = "January 20, 2006";
+let myBirthplace = "Dasay, Narvacan, Ilocos Sur, 2704";
+let myAddress = "Dasay, Narvacan, Ilocos Sur, 2704";
+let myCourse = "Bachelor of Science in Computer Science";
+let myDreamJob = "Software Engineer";
+
+
+console.log(`[${myFirstName_MiddleName_LastName.toUpperCase()}] was born [${myBirthdate}] at [${myBirthplace.toUpperCase()}]
+and currently living at [${myAddress.toUpperCase()}]  is currently taking [${myCourse.toUpperCase()}] and want to be [${My_DreamJob.toUpperCase()}]`);
+
+let theirFirstName_MiddleName_LastName1 = "Nedelyn, Fernandez, Donato";
+let theirBirthdate1 = "December 21, 2005";
+let theirBirthplace1 = "Sorioan, San Esteban, Ilocos Sur,2711";
+let theirAddress1 = "Sorioan, San Esteban, Ilocos Sur,2711";
+let theirCourse1 = "Bachelor of Science in Computer Science";
+let theirDreamJob1 = "Software Engineer";
+
+console.log(`[${theirfirstName_middleName_lastName1.toUpperCase()}] was born [${theirBirthdate1}] at [${theirBirthplace1.toUpperCase()}]
+and currently living at [${theirAddress1.toUpperCase()}] is currently taking [${theirCourse1.toUpperCase()}] and want to be [${theirDreamJob1.toUpperCase()}]`);
+
+let theirFirstName_MiddleName_LastName2 = "Krizelle Ann, Alcantara";
+let theirBirthdate2 ="November 10, 2005";
+let theirBirthplace2 = "Parioc Segundo, Candon City, Ilocos Sur,2711";
+let theirAddress2 = "Parioc Segundo, Candon City, Ilocos Sur,2711";
+let theirCourse2 = "Bachelor of Science in Computer Science";
+let theirDreamJob2 = "Business Owner";
+
+console.log(`[${theirfirstName_middleName_lastName2.toUpperCase()}] was born [${theirBirthdate2}] at [${theirBirthplace2.toUpperCase()}]
+and currently living at [${theirAddress2.toUpperCase()}] is currently taking [${theirCourse2.toUpperCase()}] and want to be [${theirDreamJob2.toUpperCase()}]`);
